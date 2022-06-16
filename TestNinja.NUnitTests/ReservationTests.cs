495f34f0c1dac1a491ab65e2d25dc69fa1624341
Ali Chavoshi <alichavoshii1372@gmail.com>
2022-06-16 12:28:12 +0430

    add nunit test

---
+++ TestNinja.NUnitTests/ReservationTests.cs
@@ -0,0 +1,43 @@
+using TestNinja.Fundamentals;
+
+namespace TestNinja.NUnitTests;
+
+public class ReservationTests
+{
+    [Test]
+    public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+    {
+        //Arrange 
+        var reservation = new Reservation();
+        //Act
+        var result = reservation.CanBeCancelledBy(new User() {IsAdmin = true});
+        //Assert
+        Assert.That(result);
+    }
+
+    [Test]
+    public void CanBeCancelledBy_UserIsOwner_ReturnTrue()
+    {
+        //Arrange
+        var reservation = new Reservation
+        {
+            //Act
+            MadeBy = new User()
+        };
+        //Act
+        var result = reservation.CanBeCancelledBy(reservation.MadeBy);
+        //Assert
+        Assert.That(result);
+    }
+
+    [Test]
+    public void CanBeCancelledBy_OtherUser_ReturnFalse()
+    {
+        //Arrange
+        var reservation = new Reservation() {MadeBy = new User()};
+        //Act
+        var result = reservation.CanBeCancelledBy(new User());
+        //Assert
+        Assert.That(!result);
+    }
+}
